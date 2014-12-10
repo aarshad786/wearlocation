5a597fb9d5dc5e59a40fc561e2b9611d816a759c
kaye wu <apiswswsw@gmail.com>
2014-12-10 02:06:25 -0800

    add location detection code.  test working on android wear 4.4w (actual hw).  although onLocationChanged() is being called constantly even when watch and phone is sitting on the desk and not moving.

--- wear/src/main/java/me/xbt/wearlocation/WatchActivity.java
+++ wear/src/main/java/me/xbt/wearlocation/WatchActivity.java
@@ -1,11 +1,32 @@
 package me.xbt.wearlocation;
 
 import android.app.Activity;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
+import android.util.Log;
 import android.widget.TextView;
 
-public class WatchActivity extends Activity {
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.wearable.Wearable;
+
+import java.util.Date;
+
+public class WatchActivity extends Activity implements
+        GoogleApiClient.ConnectionCallbacks,
+        //GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
+
+    private static final String TAG = "WatchActivity";
+    private static final long UPDATE_INTERVAL_MS = 5000L;
+    private static final long FASTEST_INTERVAL_MS = 3000L;
+
+    private GoogleApiClient mGoogleApiClient;
 
     private TextView mTextView;
 
@@ -20,5 +41,80 @@ public class WatchActivity extends Activity {
                 mTextView = (TextView) stub.findViewById(R.id.text);
             }
         });
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addApi(Wearable.API)  // used for data layer API
+                .addConnectionCallbacks(this)
+                //.addOnConnectionFailedListener(this)
+                .build();
+        //mGoogleApiClient.connect();
+
+        Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        Log.d(TAG, "last location=" + location);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mGoogleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi
+                    .removeLocationUpdates(mGoogleApiClient, this);
+        }
+        mGoogleApiClient.disconnect();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.d(TAG, "onconnected");
+
+        LocationRequest locationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(UPDATE_INTERVAL_MS)
+                .setFastestInterval(FASTEST_INTERVAL_MS);
+
+        LocationServices.FusedLocationApi
+                .requestLocationUpdates(mGoogleApiClient, locationRequest, this)
+                .setResultCallback(new ResultCallback<Status>() {
+
+                    @Override
+                    public void onResult(Status status) {
+                        if (status.getStatus().isSuccess()) {
+                            //if (Log.isLoggable(TAG, Log.DEBUG)) {
+                                Log.d(TAG, "Successfully requested location updates.  status=" + status);
+                            //}
+                        } else {
+                            Log.e(TAG,
+                                    "Failed in requesting location updates, "
+                                            + "status code: "
+                                            + status.getStatusCode()
+                                            + ", message: "
+                                            + status.getStatusMessage());
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        if (Log.isLoggable(TAG, Log.DEBUG)) {
+            Log.d(TAG, "connection to location client suspended");
+        }
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.d(TAG, "location changed: " + location);
+        mTextView.setText("location changed: " + location + "\ntime: " + new Date());
+        //addLocationEntry(location.getLatitude(), location.getLongitude());
+    }
+
 }
